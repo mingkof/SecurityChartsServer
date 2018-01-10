15ed6ce9d35e9d0d1708e566ab761a2fad12cf8e
wangziwen <417805862@qq.com>
2018-01-10 09:40:15 +0800

    增加MQ测试

---
+++ PCServer/PCServer/Areas/API/Controllers/KaKouDataJinController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PCServer;
+using SHSecurityContext.IRepositorys;
+using SHSecurityModels;
+using SHSecurityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SHSecurityServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/kakoudatajin")]
+    public class KaKouDataJinController : Controller
+    {
+        private readonly ILogger _logger;
+        // private readonly IKaKouDataHistoryJinRepository _kakoudatajin_history;
+        private readonly IKaKouDataJinRepository _kakoudatajin;
+        public KaKouDataJinController(IKaKouDataJinRepository kakoudata,ILogger<Sys110WarnController> logger)
+        {
+            _logger = logger;
+            _kakoudatajin = kakoudata;
+            // _kakoudatajin_history = kakoudata_history;
+        }
+        [HttpGet("getkakoudata/")]
+        public IActionResult GetKaKouData()
+        {
+            var query=_kakoudatajin.FindList(p => true,"",false);
+            if (query!=null)
+            {
+                return Ok(new {
+                    res=query
+                });
+                // return Ok("1111");
+            }else
+            {
+                return BadRequest("2222");
+            }
+            // return Ok("1111");
+        }
+    }
+ }
---
+++ PCServer/PCServer/Areas/API/Controllers/SysConfigController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PCServer;
+using SHSecurityContext.IRepositorys;
+using SHSecurityModels;
+using SHSecurityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SHSecurityServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/sysconfig")]
+    public class SysConfigController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ISysConfigRepository _sysConfig;
+        public SysConfigController(ISysConfigRepository sysConfig,ILogger<Sys110WarnController> logger)
+        {
+            _logger = logger;
+            _sysConfig = sysConfig;
+        }
+
+        [HttpGet("ChangeSipStatus/{value}")]
+        public IActionResult ChangeSipStatus(int value)
+        {
+            int key=(int)EConfigKey.kSipSCStatus;
+            var query=_sysConfig.Find(p=>p.key==key);
+            if (query==null)
+            {
+                _sysConfig.Add(new sys_config(){
+                    key=key,
+                    value="",
+                    valueInt=value
+                });
+            }
+            else
+            {
+                query.valueInt=value;
+                _sysConfig.Update(query);
+            }
+            return Ok("ok");
+        }
+        [HttpGet("GetSipStatus/")]
+        public IActionResult GetSipStatus()
+        {
+            int key=(int)EConfigKey.kSipSCStatus;
+            var query=_sysConfig.Find(p=>p.key==key);
+            if (query==null)
+            {
+                 _sysConfig.Add(new sys_config(){
+                    key=key,
+                    value="",
+                    valueInt=0
+                });
+                return Ok(new {
+                    res=0
+                });
+            }
+            else
+            {
+                return Ok(new {
+                    res=query.valueInt
+                });
+            }
+        }
+    }
+ }
