1ac5caa5d7e77c4fcc98a42ae71c9273cafa15a9
wangziwen <417805862@qq.com>
2018-01-18 10:16:47 +0800

    update

--- ActiveMQ/ActiveMQTest/AtiveMQTest/AtiveMQTest/MQClient/Form1.Designer.cs
+++ ActiveMQ/ActiveMQTest/AtiveMQTest/AtiveMQTest/MQClient/Form1.Designer.cs
@@ -28,26 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbReceiveMessage = new System.Windows.Forms.Label();
+            this.tbReceiveMessage1 = new System.Windows.Forms.Label();
+            this.tbReceiveMessage2 = new System.Windows.Forms.Label();
+            this.tbReceiveMessage3 = new System.Windows.Forms.Label();
+            this.tbReceiveMessage4 = new System.Windows.Forms.Label();
+            this.tbReceiveMessage5 = new System.Windows.Forms.Label();
+            this.title1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // tbReceiveMessage
+            // tbReceiveMessage1
             // 
-            this.tbReceiveMessage.AutoSize = true;
-            this.tbReceiveMessage.Location = new System.Drawing.Point(166, 75);
-            this.tbReceiveMessage.Name = "tbReceiveMessage";
-            this.tbReceiveMessage.Size = new System.Drawing.Size(41, 12);
-            this.tbReceiveMessage.TabIndex = 0;
-            this.tbReceiveMessage.Text = "label1";
+            this.tbReceiveMessage1.AutoSize = true;
+            this.tbReceiveMessage1.Location = new System.Drawing.Point(149, 62);
+            this.tbReceiveMessage1.Name = "tbReceiveMessage1";
+            this.tbReceiveMessage1.Size = new System.Drawing.Size(41, 12);
+            this.tbReceiveMessage1.TabIndex = 0;
+            this.tbReceiveMessage1.Text = "label1";
+            // 
+            // tbReceiveMessage2
+            // 
+            this.tbReceiveMessage2.AutoSize = true;
+            this.tbReceiveMessage2.Location = new System.Drawing.Point(149, 114);
+            this.tbReceiveMessage2.Name = "tbReceiveMessage2";
+            this.tbReceiveMessage2.Size = new System.Drawing.Size(41, 12);
+            this.tbReceiveMessage2.TabIndex = 1;
+            this.tbReceiveMessage2.Text = "label2";
+            // 
+            // tbReceiveMessage3
+            // 
+            this.tbReceiveMessage3.AutoSize = true;
+            this.tbReceiveMessage3.Location = new System.Drawing.Point(149, 166);
+            this.tbReceiveMessage3.Name = "tbReceiveMessage3";
+            this.tbReceiveMessage3.Size = new System.Drawing.Size(41, 12);
+            this.tbReceiveMessage3.TabIndex = 2;
+            this.tbReceiveMessage3.Text = "label3";
+            // 
+            // tbReceiveMessage4
+            // 
+            this.tbReceiveMessage4.AutoSize = true;
+            this.tbReceiveMessage4.Location = new System.Drawing.Point(149, 221);
+            this.tbReceiveMessage4.Name = "tbReceiveMessage4";
+            this.tbReceiveMessage4.Size = new System.Drawing.Size(41, 12);
+            this.tbReceiveMessage4.TabIndex = 3;
+            this.tbReceiveMessage4.Text = "label4";
+            // 
+            // tbReceiveMessage5
+            // 
+            this.tbReceiveMessage5.AutoSize = true;
+            this.tbReceiveMessage5.Location = new System.Drawing.Point(149, 272);
+            this.tbReceiveMessage5.Name = "tbReceiveMessage5";
+            this.tbReceiveMessage5.Size = new System.Drawing.Size(41, 12);
+            this.tbReceiveMessage5.TabIndex = 4;
+            this.tbReceiveMessage5.Text = "label5";
+            // 
+            // title1
+            // 
+            this.title1.AutoSize = true;
+            this.title1.Location = new System.Drawing.Point(39, 62);
+            this.title1.Name = "title1";
+            this.title1.Size = new System.Drawing.Size(53, 12);
+            this.title1.TabIndex = 5;
+            this.title1.Text = "报警通知";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "报警消除通知";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "故障通知";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "故障消除通知";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "异常通知";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 401);
-            this.Controls.Add(this.tbReceiveMessage);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.title1);
+            this.Controls.Add(this.tbReceiveMessage5);
+            this.Controls.Add(this.tbReceiveMessage4);
+            this.Controls.Add(this.tbReceiveMessage3);
+            this.Controls.Add(this.tbReceiveMessage2);
+            this.Controls.Add(this.tbReceiveMessage1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +155,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label tbReceiveMessage;
+        private System.Windows.Forms.Label tbReceiveMessage1;
+        private System.Windows.Forms.Label tbReceiveMessage2;
+        private System.Windows.Forms.Label tbReceiveMessage3;
+        private System.Windows.Forms.Label tbReceiveMessage4;
+        private System.Windows.Forms.Label tbReceiveMessage5;
+        private System.Windows.Forms.Label title1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
